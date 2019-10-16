742f0965dfa1ec3336b52c543576a1fd7a40c450
MILENA <MILENA@MILENA-PC.home>
2019-10-16 10:54:51 -0300

    Criação da classe Usuarios model.

---
+++ mavenproject1/src/main/java/model/Usuarios.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author MILENA
+ */
+public class Usuarios {
+private int id;
+private String nome;
+private String email;
+private String cpf;
+private String sexo;
+private int senha;
+private int id_filial;
+private int cargo;
+}
