b3599ef2d289719ffcfb608757d833bbefe95d8f
MILENA <MILENA@MILENA-PC.home>
2019-10-16 10:02:20 -0300

    Inclusão dos Getters e Setters.

--- mavenproject1/src/main/java/model/Clientes.java
+++ mavenproject1/src/main/java/model/Clientes.java
@@ -13,4 +13,33 @@ public class Clientes {
     private String nome;
     private int id;
     private int cpf;
+
+    //Getters e Setters Nome,Id,CPF
+   
+    public String getNome() {
+        return nome;
+    }
+
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
 }
