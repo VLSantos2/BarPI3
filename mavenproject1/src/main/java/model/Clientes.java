48498a9a8a1cc95979d3039e4bf1cbc3e4367b95
MILENA <MILENA@MILENA-PC.home>
2019-10-16 10:11:43 -0300

    Clientes model final.

--- mavenproject1/src/main/java/model/Clientes.java
+++ mavenproject1/src/main/java/model/Clientes.java
@@ -10,27 +10,29 @@ package model;
  * @author MILENA
  */
 public class Clientes {
+
     private String nome;
     private int id;
     private int cpf;
+    
+//Atributos necessarios para o cadastro de clientes.
+    public Clientes(String nome, int id, String CPF) {
 
-    //Getters e Setters Nome,Id,CPF
-   
+    }
+
+    //Getters e Setters: Nome,Id,CPF
     public String getNome() {
         return nome;
     }
 
-    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 
     public int getId() {
         return id;
     }
 
-    
     public void setId(int id) {
         this.id = id;
     }
