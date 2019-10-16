bea3df16ae40f71e6f3d75fdb4dcd83ee2831658
MILENA <MILENA@MILENA-PC.home>
2019-10-16 10:57:07 -0300

    Getters e Setters de Usuarios.

--- mavenproject1/src/main/java/model/Usuarios.java
+++ mavenproject1/src/main/java/model/Usuarios.java
@@ -10,12 +10,79 @@ package model;
  * @author MILENA
  */
 public class Usuarios {
-private int id;
-private String nome;
-private String email;
-private String cpf;
-private String sexo;
-private int senha;
-private int id_filial;
-private int cargo;
+
+    //Atributos necessários para o cadastro de usuario
+    private int id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String sexo;
+    private int senha;
+    private int id_filial;
+    private int cargo;
+
+    //Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public int getId_filial() {
+        return id_filial;
+    }
+
+    public void setId_filial(int id_filial) {
+        this.id_filial = id_filial;
+    }
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
+    }
 }
