c8c855ef00646372bba9d9cbe011389948291268
MILENA <MILENA@MILENA-PC.home>
2019-10-17 15:50:17 -0300

    Inclusão da DAO clientes.

---
+++ mavenproject1/src/main/java/dao/ClientesDao.java
@@ -0,0 +1,42 @@
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Clientes;
+
+/**
+ *
+ * @author MILENA
+ */
+
+public class ClientesDao {
+    public static ArrayList<Clientes> encontrarClientesId(int id) throws SQLException, ClassNotFoundException {
+        
+        ArrayList<Clientes> ClientesRetorno = new ArrayList<Clientes>();
+        String sql = "SELECT p.* FROM Clientes p WHERE id =?;  id = ?;";
+        
+        try (Connection conn = interface_conexao.obterConexao();
+                PreparedStatement select = conn.prepareStatement(sql);) {
+            select.setInt(1, id);
+            ResultSet retorno = select.executeQuery();
+            while (retorno.next()) {
+                Clientes p = new Clientes(
+                        retorno.getString(1),
+                        retorno.getInt(2),
+                        retorno.getInt(3),
+                        retorno.getInt(4)
+                );
+                
+                ClientesRetorno.add(p);
+            }
+            conn.close();
+        }
+        return ClientesRetorno;
+    }
+        
+        
+}
