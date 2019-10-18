95ddf3d2ee58ae62e53bb429ef0fda4ef05285e2
MiihGalvao <42485813+MiihGalvao@users.noreply.github.com>
2019-10-17 22:29:14 -0300

    Inclusão cadastrar, atualizar e excluir da DAO usuarios.

--- mavenproject1/src/main/java/dao/UsuariosDao.java
+++ mavenproject1/src/main/java/dao/UsuariosDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import model.Usuarios;
 
 /**
@@ -41,5 +42,75 @@ public class UsuariosDao {
             conn.close();
         }
         return null;
+    }
+      public boolean cadastrar(Usuarios usuarios) throws ClassNotFoundException, SQLException {
+        Connection conn = interface_conexao.obterConexao();
+        String sql = "INSERT INTO Usuarios (id, email, sexo, senha, nome, id_filial, cargo ) values(?,?,?,?,?,?,?);";
+        try (
+            PreparedStatement comando = conn.prepareStatement(sql); ){
+
+            comando.setInt(1, usuarios.getId());
+            comando.setString(2,usuarios.getEmail());
+            comando.setString(3, usuarios.getSexo());  
+            comando.setInt(4,usuarios.getSenha());
+            comando.setString(4,usuarios.getNome());
+            comando.setInt(4,usuarios.getId_filial());
+            comando.setInt(4,usuarios.getCargo());
+            
+            comando.execute();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+            
+        } finally {
+            conn.close();
+        }
+        
+        return false;
+    }
+       public boolean atualizar( Usuarios u) throws SQLException, ClassNotFoundException {
+    String sql = "UPDATE Usuarios SET nome = ?, Id = ?, dt_nascimento = ?, telefone = ?, WHERE id = ?;"; 
+    Connection conn = interface_conexao.obterConexao();
+    try (           
+            PreparedStatement comando = conn.prepareStatement(sql);){
+
+             comando.setInt(1, u.getId());
+            comando.setString(2,u.getEmail());
+            comando.setString(3, u.getSexo());  
+            comando.setInt(4,u.getSenha());
+            comando.setString(4,u.getNome());
+            comando.setInt(4,u.getId_filial());
+            comando.setInt(4,u.getCargo());
+            
+
+            comando.execute();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+
+        } finally {
+            conn.close();
+        }
+    return false;
+}
+       public boolean excluir(int id) throws ClassNotFoundException, SQLException {
+    Connection conn = interface_conexao.obterConexao();
+    String sql = "DELETE FROM usuarios WHERE id = ?;";
+        try (PreparedStatement comando = conn.prepareStatement(sql);){
+            comando.setInt(1, id);
+            
+            comando.execute();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+            
+        } finally {
+
+        }
+        
+        return false;
     }
 }
